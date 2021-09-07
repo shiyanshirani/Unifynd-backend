200eadda5635b9203a78a4df0c5e0593f4886319
shiyanshirani <shiyan99s@gmail.com>
2021-09-07 18:41:11 +0530

    minor changes

--- task/sort.py
+++ task/sort.py
@@ -2,85 +2,95 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 import csv
 
-#csv file
-csvfile = pd.read_csv("data/users.csv")
-csvfile.to_csv('output/csvusers.csv', index=False)
+def csvFileConversion():
+    csvfile = pd.read_csv("data/users.csv")
+    csvfile.to_csv('output/csvusers.csv', index=False)
 
 
-# xml file
-xmlparse = ET.parse('data/users.xml')
-root = xmlparse.getroot()
+def xmlFileConversion():
+    xmlparse = ET.parse('data/users.xml')
+    root = xmlparse.getroot()
 
-cols = ["userid", "firstname", "surname", "username", "type", "lastlogintime"]
-rows = []
+    cols = ["userid", "firstname", "surname", "username", "type", "lastlogintime"]
+    rows = []
 
-for i in root:
-    userid = i.find('userid').text
-    firstname = i.find('firstname').text
-    surname = i.find('surname').text
-    username = i.find('username').text
-    type = i.find('type').text
-    lastlogintime = i.find('lastlogintime').text
+    for i in root:
+        userid = i.find('userid').text
+        firstname = i.find('firstname').text
+        surname = i.find('surname').text
+        username = i.find('username').text
+        type = i.find('type').text
+        lastlogintime = i.find('lastlogintime').text
 
-    rows.append(
-        {
-            "userid": userid,
-            "firstname": firstname,
-            "surname": surname,
-            "username": username,
-            "type": type,
-            "lastlogintime": lastlogintime
-        }
-    )
+        rows.append(
+            {
+                "userid": userid,
+                "firstname": firstname,
+                "surname": surname,
+                "username": username,
+                "type": type,
+                "lastlogintime": lastlogintime
+            }
+        )
 
-df = pd.DataFrame(rows, columns=cols)
-df.to_csv('output/xmlusers.csv', index=False)
-
-
-#json file
-jfile = pd.read_json('data/users.json')
-jfile.to_csv('output/jsonusers.csv', index=False)
+    df = pd.DataFrame(rows, columns=cols)
+    df.to_csv('output/xmlusers.csv', index=False)
 
 
+def jsonFileConversion():
+    jfile = pd.read_json('data/users.json')
+    jfile.to_csv('output/jsonusers.csv', index=False)
 
 
-#combined csv
-data1 = 'output/csvusers.csv'
-data2 = 'output/xmlusers.csv'
-data3 = 'output/jsonusers.csv'
+def merge():
+    data1 = 'output/csvusers.csv'
+    data2 = 'output/xmlusers.csv'
+    data3 = 'output/jsonusers.csv'
 
 
-li = [data1, data2, data3]
-combined_csv = pd.concat([pd.read_csv(f) for f in li])
-combined_csv.sort_values(['userid'], axis=0, ascending=[True], inplace=True)
-combined_csv.to_csv('output/final_output.csv', index=False, encoding='utf-8-sig')
-combined_csv.to_csv('result/users.csv', index=False, encoding='utf-8-sig')
+    li = [data1, data2, data3]
+    combined_csv = pd.concat([pd.read_csv(f) for f in li])
+    combined_csv.sort_values(['userid'], axis=0, ascending=[True], inplace=True)
+    combined_csv.to_csv('output/final_output.csv', index=False, encoding='utf-8-sig')
+    combined_csv.to_csv('result/users.csv', index=False, encoding='utf-8-sig')
+
+def csvTojson():
+    outputfile = pd.read_csv('output/final_output.csv')
+    outputfile.to_json(path_or_buf='result/users.json', orient='records', lines=True)
 
 
-outputfile = pd.read_csv('output/final_output.csv')
-outputfile.to_json(path_or_buf='result/users.json', orient='records', lines=True)
+def csvToxml():
+    f = open('output/final_output.csv')
+    csv_f = csv.reader(f)
+    data = []
 
+    for row in csv_f:
+        data.append(row)
+    f.close()
 
-# converting csv to xml 
-f = open('output/final_output.csv')
-csv_f = csv.reader(f)
-data = []
+    def convert_row(row):
+        return """<Users>
+        <User>
+            <userid>%s</userid>
+            <firstname>%s</firstname>
+            <surname>%s</surname>
+            <username>%s</username>
+            <type>%s</type>
+            <lastlogintime>%s</lastlogintime>
+        </User>
+        </Users>
+        """% (row[0], row[1], row[2], row[3], row[4], row[5])
 
-for row in csv_f:
-    data.append(row)
-f.close()
+    with open('result/users.xml', 'w') as fn:
+        fn.write('\n'.join([convert_row(row) for row in data[1:]]))
 
-def convert_row(row):
-    return """<Users>
-    <User>
-        <userid>%s</userid>
-        <firstname>%s</firstname>
-        <surname>%s</surname>
-        <username>%s</username>
-        <type>%s</type>
-        <lastlogintime>%s</lastlogintime>
-    </User>
-    """% (row[0], row[1], row[2], row[3], row[4], row[5])
+def convertfiles():
+    csvFileConversion()
+    xmlFileConversion()
+    jsonFileConversion()
 
-with open('result/users.xml', 'w') as fn:
-    fn.write('\n'.join([convert_row(row) for row in data[1:]]))
+if __name__ == "__main__":
+    convertfiles()
+    merge()
+    csvTojson()
+    csvToxml()
